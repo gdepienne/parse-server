c4c5d384a1e981cb6d59abf04ca1dc0bac4b3352
Gregory Depienne <gregory.depienne@aviation-civile.gouv.fr>
2023-11-15 09:24:22 +0100

    keycloak.js modification

--- src/Adapters/Auth/keycloak.js
+++ src/Adapters/Auth/keycloak.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*
   # Parse Server Keycloak Authentication
 
@@ -8,17 +10,9 @@
       "keycloak": {
         "access_token": "access token you got from keycloak JS client authentication",
         "id": "the id retrieved from client authentication in Keycloak",
-        "roles": ["the roles retrieved from client authentication in Keycloak"],
-        "groups": ["the groups retrieved from client authentication in Keycloak"]
       }
     }
   ```
-
-  The authentication module will test if the authData is the same as the
-  userinfo oauth call, comparing the attributes
-
-  Copy the JSON config file generated on Keycloak (https://www.keycloak.org/docs/latest/securing_apps/index.html#_javascript_adapter)
-  and paste it inside of a folder (Ex.: `auth/keycloak.json`) in your server.
 
   The options passed to Parse server:
 
@@ -26,51 +20,43 @@
     {
       auth: {
         keycloak: {
-          config: require(`./auth/keycloak.json`)
+          enabled: true,
+          config: {
+            "hostname": "keycloak instance hostname",
+            "realm": "keycloak realm"
+          }
         }
       }
     }
   ```
 */
 
-const { Parse } = require('parse/node');
+const {
+  Parse
+} = require('parse/node');
 const httpsRequest = require('./httpsRequest');
 
-const arraysEqual = (_arr1, _arr2) => {
-  if (!Array.isArray(_arr1) || !Array.isArray(_arr2) || _arr1.length !== _arr2.length) return false;
-
-  var arr1 = _arr1.concat().sort();
-  var arr2 = _arr2.concat().sort();
-
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-
-  return true;
-};
-
-const handleAuth = async ({ access_token, id, roles, groups } = {}, { config } = {}) => {
+const handleAuth = async ({
+  access_token,
+  id,
+} = {}, {
+  config
+} = {}) => {
   if (!(access_token && id)) {
     throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Missing access token and/or User id');
   }
-  if (!config || !(config['auth-server-url'] && config['realm'])) {
+  if (!config || !(config['hostname'] && config['realm'])) {
     throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Missing keycloak configuration');
   }
   try {
     const response = await httpsRequest.get({
-      host: config['auth-server-url'],
-      path: `/realms/${config['realm']}/protocol/openid-connect/userinfo`,
+      host: config['hostname'],
+      path: `/auth/realms/${config['realm']}/protocol/openid-connect/userinfo`,
       headers: {
-        Authorization: 'Bearer ' + access_token,
-      },
+        Authorization: 'Bearer ' + access_token
+      }
     });
-    if (
-      response &&
-      response.data &&
-      response.data.sub == id &&
-      arraysEqual(response.data.roles, roles) &&
-      arraysEqual(response.data.groups, groups)
-    ) {
+    if (response && response.sub == id) {
       return;
     }
     throw new Parse.Error(Parse.Error.OBJECT_NOT_FOUND, 'Invalid authentication');
@@ -82,10 +68,7 @@ const handleAuth = async ({ access_token, id, roles, groups } = {}, { config } =
     if (error.error_description) {
       throw new Parse.Error(Parse.Error.HOSTING_ERROR, error.error_description);
     } else {
-      throw new Parse.Error(
-        Parse.Error.HOSTING_ERROR,
-        'Could not connect to the authentication server'
-      );
+      throw new Parse.Error(Parse.Error.HOSTING_ERROR, 'Could not connect to the authentication server');
     }
   }
 };
@@ -94,8 +77,6 @@ const handleAuth = async ({ access_token, id, roles, groups } = {}, { config } =
   @param {Object} authData: the client provided authData
   @param {string} authData.access_token: the access_token retrieved from client authentication in Keycloak
   @param {string} authData.id: the id retrieved from client authentication in Keycloak
-  @param {Array}  authData.roles: the roles retrieved from client authentication in Keycloak
-  @param {Array}  authData.groups: the groups retrieved from client authentication in Keycloak
   @param {Object} options: additional options
   @param {Object} options.config: the config object passed during Parse Server instantiation
 */
@@ -107,8 +88,7 @@ function validateAuthData(authData, options = {}) {
 function validateAppId() {
   return Promise.resolve();
 }
-
 module.exports = {
   validateAppId,
-  validateAuthData,
+  validateAuthData
 };
